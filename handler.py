79eb6017fdf18cdabdb30e5ffe28bf281e6d8e6a
Daniel <jfdaniel77@gmail.com>
2021-09-03 15:09:38 +0000

    Update submit vital sign

--- handler.py
+++ handler.py
@@ -34,18 +34,22 @@ def submit_vital_sign(event, context):
         
     payload = loads(event.get("body"))
     
-    # Connect to MongoDB Atlas
-    ssm_client = boto3.client("ssm")
-    mongodb_url = ssm_client.get_parameter(Name='serverless-mongodb-url', WithDecryption=False)
-    print("MongoDB URL =  {}".format(mongodb_url))
-    mdb_client = pymongo.MongoClient(mongodb_url)
-    print("DEBUG 1")
-    db = mdb_client.myhealth
-    print("DEBUG 2")
-    vitalsign = db.vitalsign
-    print("DEBUG 3")
-    vitalsign_id = vitalsign.insert_one(payload)
-    print("id: {}".format(vitalsign_id))
+    try:
+    
+        # Connect to MongoDB Atlas
+        ssm_client = boto3.client("ssm")
+        mongodb_url = ssm_client.get_parameter(Name='serverless-mongodb-url', WithDecryption=False)
+        print("MongoDB URL =  {}".format(mongodb_url))
+        mdb_client = pymongo.MongoClient(mongodb_url)
+        print("DEBUG 1")
+        db = mdb_client.myhealth
+        print("DEBUG 2")
+        vitalsign = db.vitalsign
+        print("DEBUG 3")
+        vitalsign_id = vitalsign.insert_one(payload)
+        print("id: {}".format(vitalsign_id))
+    except Exception as e:
+        print(e)
         
     body = {
         "message": "Submit Vital Sign for {}".format(user_id),
