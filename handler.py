9894422007184bebaf5e689f18e8fed51095d141
Daniel <jfdaniel77@gmail.com>
2021-09-03 12:26:10 +0000

    Add new service to submit vital sign

--- handler.py
+++ handler.py
@@ -18,3 +18,21 @@ def hello(event, context):
         "event": event
     }
     """
+
+def submit_vital_sign(event, context):
+    
+    user_id = ''
+    
+    # Get input parameter
+    if event.get("pathParameters") and event.get("pathParameters").get("id"):
+        user_id = event.get("pathParameters").get("id")
+    else:
+        return {"statusCode": 400, "body": "Missing User ID", "headers": {"Content-Type": "text/plain"}}
+        
+    body = {
+        "message": "Submit Vital Sign for {}".format(user_id)
+    }
+        
+    response = {"statusCode": 200, "body": json.dumps(body), "headers": {"Content-Type": "application/json"}}
+    
+    return response
